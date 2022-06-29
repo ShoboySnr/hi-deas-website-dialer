880d7c871cd02f744d11c0ab90d19edb39473303
Damilare Shobowale <oluwadamilareshobowale@gmail.com>
2022-06-29 06:05:29 +0100

    first commit

---
+++ hideas-library.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Hideas Library
+ * Plugin URI:  https://hideasng.com
+ * Description: Hideas Library for Internet Calls.
+ * Author:      Damilare Shobowale
+ * Author URI:  https://www.techwithdee.com
+ * Text Domain: hideas-library
+ * Version:     1.0.0
+ *
+ * @package Hideas_Library
+ */
