97f44f0a13cf8eb7a8b698030ae7631489fac626
Damilare Shobowale <oluwadamilareshobowale@gmail.com>
2022-07-02 17:25:19 +0100

    add floating options

--- src/Core/Shortcode.php
+++ src/Core/Shortcode.php
@@ -18,6 +18,7 @@ class Shortcode {
         add_shortcode('hi-deas-call-central', [$this, 'initialize_shortcode']);
         add_action( 'init', [$this, 'register_scripts'] );
         add_action( 'wp_enqueue_scripts', [$this, 'enqueue_scripts'] );
+        add_action( 'wp_footer', [$this, 'embed_float_options']);
     }
     
     /**
@@ -33,7 +34,7 @@ class Shortcode {
         $display_as = get_option('hideasCallCenterDisplayAs', 'image');
         $phone_text = get_option('hideasCallCentralPhoneText');
         
-        if(empty($extension) || empty($phone) || empty($hash_key)) return '';
+        if(empty($extension) || empty($phone) || empty($hash_key) || $display_as == 'floating') return '';
         
         if(empty($image_url)) $image_url = $this->default_image_url;
     
@@ -58,6 +59,45 @@ class Shortcode {
         <?php
         
         return ob_get_clean();
+    }
+    
+    public function embed_float_options() {
+        $hash_key = get_option('hideasCallCenterHashedKey');
+        $extension = get_option('hideasCallCentralExtension');
+        $phone = get_option('hideasCallCentralExtension');
+        $image_url = get_option('hideasCallCentralPhoneIconURL');
+        $display_as = get_option('hideasCallCenterDisplayAs', 'image');
+    
+        if(empty($extension) || empty($phone) || empty($hash_key) || $display_as !== 'floating') return '';
+    
+        if(empty($image_url)) $image_url = $this->default_image_url;
+    
+        $call_url = add_query_arg(['hash' => $hash_key, 'extension' => $extension, 'phone' => $phone], $this->api_url);
+    
+        $display = '<img src="'. $image_url .'" alt="" />';
+    
+        ob_start();
+    
+        ?>
+      <a href="javascript:void(0)" class="hi-deas-call-central-container"><?= $display; ?></a>
+      <noscript><?= __('You need Javascript to use the previous link or use', 'hi-deas-call-central') ?>
+        <a href="<?= $call_url; ?>" target="_blank" ><?= $display; ?></a>
+      </noscript>
+      <style>
+          a.hi-deas-call-central-container {
+              position: fixed;
+              bottom: 40px;
+              z-index: 999999999;
+              right: 20px;
+              background-color: #ffffff;
+              padding: 10px;
+              border-radius: 50%;
+          }
+      </style>
+    
+        <?php
+    
+        echo ob_get_clean();
     }
     
     
